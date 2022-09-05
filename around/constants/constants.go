9647fedcae5e42467080c77efca77b0944af69e6
zjh47981026 <zjh479810262@gmail.com>
2022-09-05 15:22:04 -0400

    update

---
+++ around/constants/constants.go
@@ -0,0 +1,10 @@
+package constants
+
+const (
+	USER_INDEX = "user"
+	POST_INDEX = "post"
+//	ES_URL      = "http://10.138.0.2:9200"
+//	ES_USERNAME = "zjh479810262"
+//	ES_PASSWORD = "Qwer19972856789"
+//	GCS_BUCKET  = "zjh479810262"
+)
